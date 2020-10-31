64ac57a50af4e6aef872e007b7acdcbdaaaa0157
Bev Hatzl <61686101+bevhatzl@users.noreply.github.com>
2020-10-31 19:49:32 +1100

    delete notes working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,10 +19,11 @@ module.exports = function (app) {
         // });
 
 
-        // console.log(noteData);
+        // Re-assign id values 
         for (i = 0; i < notes.length; i++) {
             notes[i].id = i + 1;
         }
+
         console.log(notes);
         res.json(notes);
 
@@ -34,13 +35,14 @@ module.exports = function (app) {
 
         notes.push(req.body);
 
+        //Re-assign id values
         for (i = 0; i < notes.length; i++) {
             notes[i].id = i + 1;
         }
 
-        console.log(typeof (notes));
+        // console.log(typeof (notes));
         let stringNotes = JSON.stringify(notes);
-        console.log(typeof (stringNotes));
+        // console.log(typeof (stringNotes));
         // fs.writeFile(path.join(__dirname, "../db/db.json"), parsedNotes, function (err) {
         //     if (err) return console.log(err);
         // });
@@ -55,7 +57,36 @@ module.exports = function (app) {
 
         // Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file. id should be the index of the array.
 
+        fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
+            if (err) throw err;
+            let parsedData = JSON.parse(data);
+            // console.log(parsedData);
+            // console.log(typeof (parsedData));
+            // console.log(req.params.id);
+            // console.log(parsedData[0]);
 
+            // Locate the note with the given id property
+            // let found = parsedData.find(function (item) {
+            //     return item.id == req.params.id;
+            // })
+            // console.log(found);
+
+            // Remove the note
+            notes = parsedData.filter(item => {
+                return item.id != req.params.id;
+            })
+            // console.log(filteredArray);
+
+
+        });
+
+        // Re write the file
+        let stringNotes = JSON.stringify(notes);
+        fs.writeFile(path.join(__dirname, "../db/db.json"), stringNotes, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
+
+        res.json(notes);
     });
 
 
